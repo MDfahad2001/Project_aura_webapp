e4717386637baa90b7c007d86054c07b6b7db32e
Fahad <fahadjan202001@gmail.com>
2025-06-09 18:41:54 -0400

    login page and js file

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+function login(event)
+ {
+      event.preventDefault();
+      const user = document.getElementById('username').value;
+      const pass = document.getElementById('password').value;
+
+      if (user === 'admin' && pass === 'password') {
+        window.location.href = 'dashboard.html';
+      } else {
+        alert('Invalid credentials');
+      }
+}
